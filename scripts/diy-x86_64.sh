146e21baeaf92b2a031a1082e9f0465b34889c12
grandway2025 <zouchanggan@gmail.com>
2025-06-02 16:57:28 +0800

    Update diy-x86_64.sh

--- scripts/diy-x86_64.sh
+++ scripts/diy-x86_64.sh
@@ -363,7 +363,7 @@ git clone https://$github/grandway2025/OpenAppFilter package/new/OpenAppFilter
 # adguardhome
 git clone https://$github/grandway2025/luci-app-adguardhome package/new/luci-app-adguardhome
 mkdir -p files/usr/bin
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_amd64 | awk -F '"' '{print $4}')
 wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
 chmod +x files/usr/bin/AdGuardHome
 
