1be2d38a970bec930208e946a3278bfa97c7b6bc
grandway2025 <zouchanggan@gmail.com>
2025-08-31 13:32:54 +0800

    Update diy-x86_64.sh

--- scripts/diy-x86_64.sh
+++ scripts/diy-x86_64.sh
@@ -29,40 +29,40 @@ sed -ie 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/'
 grep HASH include/kernel-6.6 | awk -F'HASH-' '{print $2}' | awk '{print $1}' | md5sum | awk '{print $1}' > .vermagic
 
 # docker
-[ "$ENABLE_DOCKER" = "y" ] && cp ../configs/config-docker >> .config
+[ "$ENABLE_DOCKER" = "y" ] && cat ../configs/config-docker >> .config
 
 # ShadowSocksR Plus
-[ "$ENABLE_SSRP" = "y" ] && cp ../configs/config-ssrp >> .config
+[ "$ENABLE_SSRP" = "y" ] && cat ../configs/config-ssrp >> .config
 
 # passwall
-[ "$ENABLE_PASSWALL" = "y" ] && cp ../configs/config-passwall >> .config
+[ "$ENABLE_PASSWALL" = "y" ] && cat ../configs/config-passwall >> .config
 
 # nikki
-[ "$ENABLE_NIKKI" = "y" ] && cp ../configs/config-nikki >> .config
+[ "$ENABLE_NIKKI" = "y" ] && cat ../configs/config-nikki >> .config
 
 # openclash
-[ "$ENABLE_OPENCLASH" = "y" ] && cp ../configs/config-openclash >> .config
+[ "$ENABLE_OPENCLASH" = "y" ] && cat ../configs/config-openclash >> .config
 
 # AdGuard Home
-# [ "$ENABLE_ADGUARDHOME" = "y" ] && cp ../configs/config-adguardhome >> .config
+# [ "$ENABLE_ADGUARDHOME" = "y" ] && cat ../configs/config-adguardhome >> .config
 
 # Lucky
-[ "$ENABLE_LUCKY" = "y" ] && cp ../configs/config-lucky >> .config
+[ "$ENABLE_LUCKY" = "y" ] && cat ../configs/config-lucky >> .config
 
 # OpenAppFilter
-[ "$ENABLE_OAF" = "y" ] && cp ../configs/config-oaf >> .config
+[ "$ENABLE_OAF" = "y" ] && cat ../configs/config-oaf >> .config
 
 # MosDNS
-# [ "$ENABLE_MOSDNS" = "y" ] && cp ../configs/config-mosdns >> .config
+# [ "$ENABLE_MOSDNS" = "y" ] && cat ../configs/config-mosdns >> .config
 
 # OpenList
-# [ "$ENABLE_OPENLIST" = "y" ] && cp ../configs/config-openlist >> .config
+# [ "$ENABLE_OPENLIST" = "y" ] && cat ../configs/config-openlist >> .config
 
 # 任务设置
-# [ "$ENABLE_TASKPLAN" = "y" ] && cp ../configs/config-taskplan >> .config
+# [ "$ENABLE_TASKPLAN" = "y" ] && cat ../configs/config-taskplan >> .config
 
 # 高级设置
-# [ "$ENABLE_ADVANCEDPLUS" = "y" ] && cp ../configs/config-advancedplus >> .config
+# [ "$ENABLE_ADVANCEDPLUS" = "y" ] && cat ../configs/config-advancedplus >> .config
 
 # 移除 SNAPSHOT 标签
 sed -i 's,-SNAPSHOT,,g' include/version.mk
@@ -124,32 +124,14 @@ sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' 
 # cp ../Customize/base-files/banner > package/base-files/files/etc/banner
 
 # make olddefconfig
-cp ../doc/patch/kernel-6.6/kernel/0003-include-kernel-defaults.mk.patch | patch -p1
+# cp ../doc/patch/kernel-6.6/kernel/0003-include-kernel-defaults.mk.patch | patch -p1
+patch -p1 < ../doc/patch/kernel-6.6/kernel/0003-include-kernel-defaults.mk.patch
 
 # bbr
-pushd target/linux/generic/backport-6.6
-    cp ../doc/patch/kernel-6.6/bbr3/010-0001-net-tcp_bbr-broaden-app-limited-rate-sample-detectio.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0002-net-tcp_bbr-v2-shrink-delivered_mstamp-first_tx_msta.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0003-net-tcp_bbr-v2-snapshot-packets-in-flight-at-transmi.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0004-net-tcp_bbr-v2-count-packets-lost-over-TCP-rate-samp.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0005-net-tcp_bbr-v2-export-FLAG_ECE-in-rate_sample.is_ece.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0006-net-tcp_bbr-v2-introduce-ca_ops-skb_marked_lost-CC-m.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0007-net-tcp_bbr-v2-adjust-skb-tx.in_flight-upon-merge-in.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0008-net-tcp_bbr-v2-adjust-skb-tx.in_flight-upon-split-in.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0009-net-tcp-add-new-ca-opts-flag-TCP_CONG_WANTS_CE_EVENT.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0010-net-tcp-re-generalize-TSO-sizing-in-TCP-CC-module-AP.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0011-net-tcp-add-fast_ack_mode-1-skip-rwin-check-in-tcp_f.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0012-net-tcp_bbr-v2-record-app-limited-status-of-TLP-repa.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0013-net-tcp_bbr-v2-inform-CC-module-of-losses-repaired-b.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0014-net-tcp_bbr-v2-introduce-is_acking_tlp_retrans_seq-i.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0015-tcp-introduce-per-route-feature-RTAX_FEATURE_ECN_LOW.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0016-net-tcp_bbr-v3-update-TCP-bbr-congestion-control-mod.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0017-net-tcp_bbr-v3-ensure-ECN-enabled-BBR-flows-set-ECT-.patch
-    cp ../doc/patch/kernel-6.6/bbr3/010-0018-tcp-export-TCPI_OPT_ECN_LOW-in-tcp_info-tcpi_options.patch
-popd
+cp ../doc/patch/kernel-6.6/bbr3/*.patch target/linux/generic/backport-6.6/
 
 # LRNG
-echo '
+cp ../doc/patch/kernel-6.6/lrng/*.patch target/linux/generic/hack-6.6/
 # CONFIG_RANDOM_DEFAULT_IMPL is not set
 CONFIG_LRNG=y
 CONFIG_LRNG_DEV_IF=y
@@ -160,35 +142,6 @@ CONFIG_LRNG_CPU=y
 CONFIG_LRNG_SELFTEST=y
 # CONFIG_LRNG_SELFTEST_PANIC is not set
 ' >>./target/linux/generic/config-6.6
-pushd target/linux/generic/hack-6.6
-    cp ../doc/patch/kernel-6.6/lrng/696-01-v57-0001-LRNG-Entropy-Source-and-DRNG-Manager.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-02-v57-0002-LRNG-allocate-one-DRNG-instance-per-NUMA-node.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-03-v57-0003-LRNG-proc-interface.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-04-v57-0004-LRNG-add-switchable-DRNG-support.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-05-v57-0005-LRNG-add-common-generic-hash-support.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-06-v57-0006-crypto-DRBG-externalize-DRBG-functions-for-LRNG.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-07-v57-0007-LRNG-add-SP800-90A-DRBG-extension.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-08-v57-0008-LRNG-add-kernel-crypto-API-PRNG-extension.patch
-    cp ..r/doc/patch/kernel-6.6/lrng/696-09-v57-0009-LRNG-add-atomic-DRNG-implementation.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-10-v57-0010-LRNG-add-common-timer-based-entropy-source-code.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-11-v57-0011-LRNG-add-interrupt-entropy-source.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-12-v57-0012-scheduler-add-entropy-sampling-hook.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-13-v57-0013-LRNG-add-scheduler-based-entropy-source.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-14-v57-0014-LRNG-add-SP800-90B-compliant-health-tests.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-15-v57-0015-LRNG-add-random.c-entropy-source-support.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-16-v57-0016-LRNG-CPU-entropy-source.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-17-v57-0017-LRNG-add-Jitter-RNG-fast-noise-source.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-18-v57-0018-LRNG-add-option-to-enable-runtime-entropy-rate-c.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-19-v57-0019-LRNG-add-interface-for-gathering-of-raw-entropy.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-20-v57-0020-LRNG-add-power-on-and-runtime-self-tests.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-21-v57-0021-LRNG-sysctls-and-proc-interface.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-22-v57-0022-LRMG-add-drop-in-replacement-random-4-API.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-23-v57-0023-LRNG-add-kernel-crypto-API-interface.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-24-v57-0024-LRNG-add-dev-lrng-device-file-support.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-25-v57-0025-LRNG-add-hwrand-framework-interface.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-26-v57-01-config_base_small.patch
-    cp ../doc/patch/kernel-6.6/lrng/696-27-v57-02-sysctl-unconstify.patch
-popd
 
 # firewall4
 mkdir -p package/network/config/firewall4/patches
@@ -362,13 +315,18 @@ sed -i 's/env conf_inc/env conf_inc enable/g' feeds/packages/net/frp/files/frpc.
 sed -i "s/'conf_inc:list(string)'/& \\\\/" feeds/packages/net/frp/files/frpc.init
 sed -i "/conf_inc:list/a\\\t\t\'enable:bool:0\'" feeds/packages/net/frp/files/frpc.init
 sed -i '/procd_open_instance/i\\t\[ "$enable" -ne 1 \] \&\& return 1\n' feeds/packages/net/frp/files/frpc.init
-cp ../Customize/frpc/001-luci-app-frpc-hide-token.patch | patch -p1
-cp ../Customize/frpc/002-luci-app-frpc-add-enable-flag.patch | patch -p1
+# cp ../Customize/frpc/001-luci-app-frpc-hide-token.patch | patch -p1
+# cp ../Customize/frpc/002-luci-app-frpc-add-enable-flag.patch | patch -p1
+patch -p1 < ../Customize/frpc/001-luci-app-frpc-hide-token.patch
+patch -p1 < ../Customize/frpc/002-luci-app-frpc-add-enable-flag.patch
 
 # natmap
 sed -i 's/log_stdout:bool:1/log_stdout:bool:0/g;s/log_stderr:bool:1/log_stderr:bool:0/g' feeds/packages/net/natmap/files/natmap.init
+# pushd feeds/luci
+#     cp ../Customize/natmap/0001-luci-app-natmap-add-default-STUN-server-lists.patch | patch -p1
+# popd
 pushd feeds/luci
-    cp ../Customize/natmap/0001-luci-app-natmap-add-default-STUN-server-lists.patch | patch -p1
+    patch -p1 < ../../Customize/natmap/0001-luci-app-natmap-add-default-STUN-server-lists.patch
 popd
 
 # samba4 - bump version
