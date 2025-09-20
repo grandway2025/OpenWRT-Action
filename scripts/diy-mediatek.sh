6e77ef1ecbe4f0b9a9e1fbd329bbbe79da1fe02e
grandway2025 <zouchanggan@gmail.com>
2025-09-20 19:58:51 +0800

    Update diy-mediatek.sh

--- scripts/diy-mediatek.sh
+++ scripts/diy-mediatek.sh
@@ -32,11 +32,15 @@ git clone https://github.com/grandway2025/argon package/new/luci-theme-argon --d
 #添加额外软件包
 # golang 1.25
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+git clone https://git.kejizero.online/zhao/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # SSRP & Passwall
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone -b openwrt-24.10 https://github.com/grandway2025/helloworld package/new/helloworld
+git clone https://github.com/sbwml/openwrt_helloworld package/new/helloworld -b v5 --depth=1
+
+# PowerOff 关机插件
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/new/poweroff
+mv -n package/new/poweroff/luci-app-poweroffdevice package/new/luci-app-poweroffdevice && rm -rf package/new/poweroff
 
 # lucky
 git clone https://github.com/gdy666/luci-app-lucky.git package/new/lucky
@@ -47,53 +51,62 @@ cp -r package/new/mosdns/{luci-app-mosdns,mosdns,v2dat} package/new/helloworld
 rm -rf package/new/mosdns
 
 # OpenAppFilter
-git clone https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter --depth=1
+git clone https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
 
 # luci-app-taskplan
-git clone https://github.com/sirpdboy/luci-app-taskplan package/new/luci-app-taskplan --depth=1
+git clone https://github.com/sirpdboy/luci-app-taskplan package/new/luci-app-taskplan
 
 # luci-app-webdav
-git clone -b openwrt-24.10 https://github.com/sbwml/luci-app-webdav.git package/new/luci-app-webdav --depth=1
-
-# luci-app-quickfile
-git clone https://$github/sbwml/luci-app-quickfile package/new/quickfile --depth=1
+git clone -b openwrt-24.10 https://github.com/sbwml/luci-app-webdav.git package/new/luci-app-webdav
 
 # openlist
 rm -rf feeds/luci/applications/luci-app-openlist
-git clone https://$github/sbwml/luci-app-openlist2 package/new/openlist --depth=1
+git clone https://github.com/sbwml/luci-app-openlist2 package/new/openlist --depth=1
 
 # socat
-git clone https://github.com/zhiern/luci-app-socat package/new/luci-app-socat --depth=1
+git clone https://github.com/zhiern/luci-app-socat package/new/luci-app-socat
 
 # luci-app-advancedplus
 # git clone https://$github/sirpdboy/luci-app-advancedplus.git package/new/luci-app-advancedplus --depth=1
 
-# adguardhome
-git clone https://git.kejizero.online/zhao/luci-app-adguardhome package/new/luci-app-adguardhome --depth=1
-mkdir -p files/usr/bin
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_arm64 | awk -F '"' '{print $4}')
-wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
-chmod +x files/usr/bin/AdGuardHome
-
 # openclash
 mkdir -p files/etc/openclash/core
+mkdir -p files/etc/config
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+CLASH_URL="https://github.com/grandway2025/Actions-OpenWrt/raw/refs/heads/main/files/openclash"
+NIKKI_URL="https://github.com/grandway2025/Actions-OpenWrt/raw/refs/heads/main/files/nikki"
 wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+wget -qO- $CLASH_URL > files/etc/config/openclash
+wget -qO- $NIKKI_URL > files/etc/config/nikki
 chmod +x files/etc/openclash/core/clash*
+chmod +x files/etc/config/openclash
+chmod +x files/etc/config/nikki
+
+# adguardhome
+git clone https://git.kejizero.online/zhao/luci-app-adguardhome package/new/luci-app-adguardhome
+mkdir -p files/usr/bin
+AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_arm64 | awk -F '"' '{print $4}')
+AGH_YAML="https://github.com/grandway2025/Actions-OpenWrt/raw/refs/heads/main/files/AdGuardHome.yaml"
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
+chmod +x files/usr/bin/AdGuardHome
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
+wget -qO- $AGH_YAML > files/etc/AdGuardHome.yaml
+chmod +x files/usr/bin/AdGuardHome
+chmod +x files/etc/AdGuardHome.yaml
 
 # Docker
 rm -rf feeds/luci/applications/luci-app-dockerman
-git clone https://github.com/sirpdboy/luci-app-dockerman.git package/new/dockerman --depth=1
+git clone https://github.com/sirpdboy/luci-app-dockerman.git package/new/dockerman
 mv -n package/new/dockerman/luci-app-dockerman feeds/luci/applications && rm -rf package/new/dockerman
-    rm -rf feeds/packages/utils/{docker,dockerd,containerd,runc}
-    git clone https:///github.com/sbwml/packages_utils_docker feeds/packages/utils/docker --depth=1
-    git clone https:///github.com/sbwml/packages_utils_dockerd feeds/packages/utils/dockerd --depth=1
-    git clone https:///github.com/sbwml/packages_utils_containerd feeds/packages/utils/containerd --depth=1
-    git clone https:///github.com/sbwml/packages_utils_runc feeds/packages/utils/runc --depth=1
+rm -rf feeds/packages/utils/{docker,dockerd,containerd,runc}
+git clone https://github.com/sbwml/packages_utils_docker feeds/packages/utils/docker
+git clone https://github.com/sbwml/packages_utils_dockerd feeds/packages/utils/dockerd
+git clone https://github.com/sbwml/packages_utils_containerd feeds/packages/utils/containerd
+git clone https://github.com/sbwml/packages_utils_runc feeds/packages/utils/runc
 
 # default-settings 
 rm -rf package/emortal/default-settings
